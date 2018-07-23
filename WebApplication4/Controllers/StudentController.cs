7d867bc255cc545f27db61e0aa33dd3b495b26ab
shrah <shrah@DESKTOP-VUBB60S>
2018-07-23 15:06:22 -0600

    Add project files.

---
+++ WebApplication4/Controllers/StudentController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Models;
+
+
+namespace WebApplication4.Controllers
+{
+    public class StudentController : Controller
+    {
+        IList<Student> studentList = new List<Student>() {
+                            new Student() { StudentId = 1, StudentName = "John", Age = 18 } ,
+                            new Student() { StudentId = 2, StudentName = "Steve",  Age = 21 } ,
+                            new Student() { StudentId = 3, StudentName = "Bill",  Age = 25 } ,
+                            new Student() { StudentId = 4, StudentName = "Ram" , Age = 20 } ,
+                            new Student() { StudentId = 5, StudentName = "Ron" , Age = 31 } ,
+                            new Student() { StudentId = 4, StudentName = "Chris" , Age = 17 } ,
+                            new Student() { StudentId = 4, StudentName = "Rob" , Age = 19 }
+                        };
+        //GET: Student
+        public ActionResult Index()
+        {
+            return View(studentList);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var std = studentList.Where(s => s.StudentId == id).FirstOrDefault();
+
+            return View(std); 
+        }
+        [HttpPost]
+        public ActionResult Edit(Student s)
+        {
+            //Write code to update the Student 
+            var name = s.StudentName;
+            var age = s.Age;
+            
+            return RedirectToAction("Index"); 
+        }
+
+        public ActionResult Details(Student s)
+        {
+            return RedirectToAction("Index"); 
+        }
+        //[NonAction]
+        //public string Index()
+        //{
+        //    return "Entering in the Student Controller Index Method"; 
+        //}
+    }
+}
---
+++ WebApplication4/Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        [Display(Name="Name")]
+        public string StudentName { get; set;  }
+
+        public int Age { set; get; }
+        //public Standard standard { get; set; }
+    }
+
+    public class Standard
+    {
+        public int StandardId { get; set; }
+
+        public string StandardName { get; set; }
+    }
+
+}
